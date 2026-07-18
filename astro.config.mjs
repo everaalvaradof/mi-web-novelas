f2e881d7662202e1cb736da757ac80f5ecc5adfc
everaalvaradof <alvaradofernandez1989@gmail.com>
2026-07-18 13:55:46 -0400

    mejorar filtro de generos y base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://everaalvaradof.github.io',
-  base: '/mi-web-novelas/', // Esto es vital, pon el nombre de tu repo aquí
+  base: '/mi-web-novelas/', // Asegúrate de la barra al final
 });
