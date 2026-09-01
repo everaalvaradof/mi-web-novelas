7c829d5fee24f87a95c84a7aa57892947ffa2551
everaalvaradof <alvaradofernandez1989@gmail.com>
2026-09-01 14:48:20 -0400

    sitemap-index.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
+import sitemap from '@astrojs/sitemap'; // <--- 1. Importas el sitemap
 
 export default defineConfig({
   site: 'https://novelasligeras.netlify.app',
   output: 'server',
-  // Esto hace que el adaptador de Netlify solo actúe al compilar, 
-  // evitando que joda con las Edge Functions en tu localhost.
   adapter: process.env.NODE_ENV === 'production' ? netlify({ imageCDN: false }) : undefined,
+  integrations: [sitemap()], // <--- 2. Lo agregas aquí en las integraciones
 });
