7944833825a6b998ab67e4dfe8e838acc1d19edf
everaalvaradof <alvaradofernandez1989@gmail.com>
2026-09-01 09:54:27 -0400

    Configurar adaptador de Netlify y modo SSR dinámico

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   site: 'https://novelasligeras.netlify.app',
   output: 'server',
   adapter: netlify({
-    // Forzamos a que el bundle de la función sea lean (ligero) y no empaquete de más
     imageCDN: false
   }),
-  integrations: [sitemap()],
 });
