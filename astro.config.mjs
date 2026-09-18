c4942ecda1dbb8bb409a1678f2f96f0f17cf1f64
everaalvaradof <alvaradofernandez1989@gmail.com>
2026-09-18 16:00:39 -0400

    2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
-import sitemap from '@astrojs/sitemap'; // <--- 1. Importas el sitemap
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://novelasligeras.netlify.app',
   output: 'server',
-  adapter: process.env.NODE_ENV === 'production' ? netlify({ imageCDN: false }) : undefined,
-  integrations: [sitemap()], // <--- 2. Lo agregas aquí en las integraciones
+  adapter: process.env.NODE_ENV === 'production' ? netlify({ 
+    imageCDN: false,
+    // Aquí le decimos que ignore la carpeta de datos y capítulos para que la función no pese gigabytes
+    includeFiles: ['./src/data/novelas.js'] 
+  }) : undefined,
+  integrations: [sitemap()],
 });
