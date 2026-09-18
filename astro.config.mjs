a735b732f4767f3467a2152f9ef29e3589e6dc9a
everaalvaradof <alvaradofernandez1989@gmail.com>
2026-09-18 16:10:42 -0400

    2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://novelasligeras.netlify.app',
-  output: 'server',
-  adapter: netlify(),
+  output: 'static', // <--- Cambiado a estático para evitar funciones pesadas
   integrations: [sitemap()],
 });
