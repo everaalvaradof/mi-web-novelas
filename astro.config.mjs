2e4b27059040b8bd7dec3a3c5902c6cb9ae7a1a9
everaalvaradof <alvaradofernandez1989@gmail.com>
2026-09-18 16:04:58 -0400

    2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,6 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://novelasligeras.netlify.app',
   output: 'server',
-  adapter: process.env.NODE_ENV === 'production' ? netlify({ 
-    imageCDN: false,
-    // Aquí le decimos que ignore la carpeta de datos y capítulos para que la función no pese gigabytes
-    includeFiles: ['./src/data/novelas.js'] 
-  }) : undefined,
+  adapter: netlify(),
   integrations: [sitemap()],
 });
