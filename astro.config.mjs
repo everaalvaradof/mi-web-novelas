ed5004ad73bbc523f70e68b99648531d5f442cd4
everaalvaradof <alvaradofernandez1989@gmail.com>
2026-09-19 14:40:51 -0400

    2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server', // Cambiamos a server (o hybrid) para que use el adaptador de Netlify
-  adapter: netlify(),
+  output: 'static', // Esto le dice a Astro que genere HTMLs puros, NO funciones de servidor.
 });
