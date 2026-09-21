d831941eb88de917707398f869b9c77276a70935
everaalvaradof <alvaradofernandez1989@gmail.com>
2026-09-21 00:38:56 -0400

    indexnow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,20 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
+import indexnow from 'astro-indexnow';
 
 export default defineConfig({
-  output: 'server', // O 'hybrid' si combinas páginas estáticas y dinámicas
+  site: 'https://novelasligeras.vercel.app',
+  output: 'server',
   adapter: vercel(),
+  integrations: [
+    indexnow({
+      host: 'novelasligeras.vercel.app',
+      key: 'a739adb17do924676a7038bmi5mib7do703a',
+      // Opcional: puedes listar tus URLs principales o dejar que el sitemap las envíe
+      urlList: [
+        'https://novelasligeras.vercel.app/',
+        // Agrega aquí las URLs clave de tus estrenos o categorías si deseas forzarlas de una vez
+      ]
+    }),
+  ],
 });
